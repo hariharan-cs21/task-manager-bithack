ee59ff11de0acadbc8cc96c9025ed03a1ad567a0
Hariharan <hariharant.cs21@bitsathy.ac.in>
2023-08-14 11:27:27 +0530

    notification bug cleared

--- src/components/Notify.js
+++ src/components/Notify.js
@@ -4,7 +4,10 @@ const NotificationBar = ({ tasks, currentUserEmail }) => {
     const [showDetails, setShowDetails] = useState(false);
     const [selectedTask, setSelectedTask] = useState(null);
 
-    const otherUserTasks = tasks.filter(task => task.assignedTo !== currentUserEmail);
+    const isAdmin = currentUserEmail === 'linktothedeveloper@gmail.com';
+
+    const userTasks = tasks.filter(task => task.assignedTo === currentUserEmail);
+    const allTasks = isAdmin ? tasks : [];
 
     const handleTaskClick = (task) => {
         setSelectedTask(task);
@@ -12,6 +15,7 @@ const NotificationBar = ({ tasks, currentUserEmail }) => {
     };
 
     const handleCloseDetails = () => {
+        setSelectedTask(null);
         setShowDetails(false);
     };
 
@@ -20,29 +24,32 @@ const NotificationBar = ({ tasks, currentUserEmail }) => {
             className="fixed m-4 ml-1  ml-1 max-w-lg mx-auto p-4 rounded-md bg-blue-500 text-white shadow-lg"
             style={{ maxHeight: '80vh', overflowY: 'auto', width: '100%' }}
         >
-            {showDetails ? (
+            {showDetails && selectedTask ? (
                 <>
-                    <h3 className="text-lg font-semibold mb-2">{selectedTask?.Task}</h3>
-                    <p className="mb-4">{selectedTask?.description}</p>
-                    <p className="text-xs text-gray-300 mb-2">Assigned To: {selectedTask?.assignedTo}</p>
+                    <h3 className="text-lg font-semibold mb-2">{selectedTask.Task}</h3>
+                    <p className="mb-4">{selectedTask.description}</p>
+                    <p className="text-xs text-gray-300 mb-2">
+                        Assigned To: {isAdmin ? selectedTask.assignedTo : 'You'}
+                    </p>
                     <button
                         className="bg-white text-blue-500 px-3 py-1 rounded-full text-xs font-semibold"
                         onClick={handleCloseDetails}
+                        aria-label="Close Details"
                     >
                         Close Details
                     </button>
                 </>
             ) : (
                 <div>
-                    <p>{otherUserTasks.length} task(s) assigned recently:</p>
+                    <p>{isAdmin ? allTasks.length : userTasks.length} task(s) assigned recently:</p>
                     <ul>
-                        {otherUserTasks.map(task => (
+                        {(isAdmin ? allTasks : userTasks).map(task => (
                             <li key={task.id}>
                                 <button
                                     className="text-left w-full py-1 font-semibold focus:outline-none"
                                     onClick={() => handleTaskClick(task)}
                                 >
-                                    {task.Task} - Assigned To: {task.assignedTo}
+                                    {task.Task} - Assigned To: {isAdmin ? task.assignedTo : 'You'}
                                 </button>
                             </li>
                         ))}
